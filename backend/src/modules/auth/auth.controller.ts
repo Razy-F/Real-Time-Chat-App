bc35f4b98c7aee6f7af69cc16250a83b90cc34bb
Razy F <105067767+Razy-F@users.noreply.github.com>
2025-10-09 23:56:06 +0300

    Check for existing email, hash password, create new user

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,10 +1,26 @@
 import { Response, Request } from "express";
 import z from "zod";
 import { SignUpBody, signUpSchema } from "./auth.validation";
+import { User } from "../user/user.model";
+import { genSalt, hash } from "bcryptjs";
 
-export function signUp(req: Request<{}, {}, SignUpBody>, res: Response) {
+export async function signUp(req: Request<{}, {}, SignUpBody>, res: Response) {
   try {
     const { fullName, email, password } = signUpSchema.parse(req.body);
+
+    const user = await User.findOne({
+      email,
+    });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
   } catch (error) {
     if (error instanceof Error)
       return res.status(400).json({ message: error.message });
