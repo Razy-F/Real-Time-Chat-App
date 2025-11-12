3501e3de2264f31c5d5cf40886dae89e6b22b351
Razy F <105067767+Razy-F@users.noreply.github.com>
2025-11-12 17:36:44 +0200

    Added the cookie-parser middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import authRoutes from "~/routes/auth.route";
+import authRoutes from "~/modules/auth/auth.route";
+import cookieParser from "cookie-parser";
 import { connectToDB } from "./lib/db";
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // By that we can get access to the fields that users sends ( its going to be under the req.body )
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
