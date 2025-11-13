d7091984ebbd05bed1b2730d5f66378a2ead33f3
Razy F <105067767+Razy-F@users.noreply.github.com>
2025-11-14 00:02:51 +0200

    Developed the user protection route (middleware)

--- backend/src/modules/user/user.middleware.ts
+++ backend/src/modules/user/user.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import env from "~/utils/envValidation";
+import { User } from "./user.model";
 export async function protectRoute(
   req: Request,
   res: Response,
@@ -16,5 +17,14 @@ export async function protectRoute(
     const decoded = jwt.verify(token, env.JWT_SECRET);
     if (!decoded)
       return res.status(401).json({ message: "Unauthorized - Invalid token" });
-  } catch (error) {}
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Error in protectRoute middleware:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
