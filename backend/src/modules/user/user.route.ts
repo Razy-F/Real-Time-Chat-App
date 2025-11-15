5cd0f9b6486542e7615100e9f57ceda5ae300213
Razy F <105067767+Razy-F@users.noreply.github.com>
2025-11-15 21:07:32 +0200

    Add user protection middleware to signup route

--- backend/src/modules/user/user.route.ts
+++ backend/src/modules/user/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { updateProfile } from "./user.controller";
+import { protectRoute } from "./user.middleware";
 
 const router = express.Router();
 // We acually prefixes the mentioned endpoints with '/api/auth' -  app.use("/api/auth", authRoutes);
 
-router.get("/signup", updateProfile);
+router.put("/signup", protectRoute, updateProfile);
 
 export default router;
