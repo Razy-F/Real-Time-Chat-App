53f5a9316de3c4a78fc126f33dffeb2eea64a8e7
Razy F <105067767+Razy-F@users.noreply.github.com>
2026-04-01 00:52:02 +0300

    feat(auth): implement JWT protected route middleware

--- backend/src/modules/auth/auth.middleware.ts
+++ backend/src/modules/auth/auth.middleware.ts
@@ -1,4 +1,8 @@
-import { NextFunction, Response, Request } from "express";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import env from "~/utils/envValidation";
+import { User } from "../user/user.model";
+
 export async function protectRoute(
   req: Request,
   res: Response,
@@ -22,6 +26,16 @@ export async function protectRoute(
       return res.status(401).json({
         message: "Unauthorized - Invalid token",
       });
+    }
+
+    if (!decoded)
+      return res.status(401).json({ message: "Unauthorized - Invalid token" });
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    req.user = user;
+    next();
   } catch (error) {
     console.error("Error in protectRoute middleware:", error);
     res.status(500).json({ message: "Internal server error" });
